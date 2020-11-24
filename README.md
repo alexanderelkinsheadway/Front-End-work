7e41a260b2c6df38b37eb98220a922158356a4dc
alexanderelkinsheadway <74934300+alexanderelkinsheadway@users.noreply.github.com>
2020-11-24 11:16:01 -0800

    Start Start

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Front-End-work
+
+Start
 Created with CodeSandbox
--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import App from "./App";
+import { gql } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://unjld.sse.codesandbox.io/",
+  uri: "https://r8qjn.sse.codesandbox.io/",
   cache: new InMemoryCache()
 });
+
+client
+  .query({
+    query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then((result) => console.log(result));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
